9f049409058ae68e00c6f4636137b417bded5229
Albin Biju <albin.biju@experionglobal.local>
2023-12-15 13:46:38 +0530

    fix: ui updated

--- script/game.js
+++ script/game.js
@@ -33,7 +33,7 @@ let answers = new Map([
 
     [5, "Asia"],
 
-    [6, "Asia"],
+    [6, "Africa"],
 
     [7, "Europe"],
 
@@ -43,6 +43,20 @@ let answers = new Map([
 
     [10, "South America"]
 ]);
+
+let descriptions= new Map([
+    [1,"India is located in the southern part of Asia."],
+    [2,"Asia is the largest continent with a land area of 44.48 million km sq."],
+    [3,"Australia is the smallest with a land area of 7686884 km sq."],
+    [4,"Since Australia lies between the Pacific and the Indian Ocean, it is called as the 'Island Continent'."],
+    [5,"Mt. Everest is located in Asia precisely in the Khumbu region of Nepal."],
+    [6,"Egypt is located in African continent having coastlines on the Mediterranean Sea, the River Nile, and the Red Sea."],
+    [7,"France is located in the western edge of Europe."],
+    [8,"Africa, the second-largest continent, covers an area more than three times that of the United States."],
+    [9,"Christopher Columbus founded North America.s known for his 1492 'discovery' of the 'new world' of the Americas on board his ship Santa Maria."],
+    [10,"The Amazon River is located in the northern portion of South America, flowing from west to east."]
+ 
+])
 
 
 //variable declarations
@@ -100,6 +114,8 @@ function generateRandomNumber()
 //function for generating random questions and displaying it
 
 function generateRandomQuestion(){
+    document.getElementById('map-image').style.pointerEvents = 'auto';
+    console.log(count);
     if(index<=count)
     {
         console.log(index);
@@ -124,8 +140,9 @@ function generateRandomQuestion(){
     }
     else
     {
-        alert(`${localStorage.getItem('user')}, you have scored ${(mark/count)*100}`);
+        alert(`${localStorage.getItem('user')}, you have scored ${Math.round((mark/count)*100)}% mark`);
     }
+    document.querySelector('#description').textContent="";
 }
 
 //function for redirecting to animation page 
@@ -143,24 +160,47 @@ function generateResult()
 }
 
 //function for checking whether answer is correct on map click
-
+let keyw;
 function checkAnswer(answer){
+    document.getElementById('map-image').style.pointerEvents = 'none';
     if(answer === answers.get(randomNo)){
         mark++;
         console.log(mark);
         let displaystatus=document.getElementById('answer1');
         displaystatus.style.color="green";
         displaystatus.innerHTML="Correct";
+        keyw=1;
     }
     
     else{
         let displaystatus=document.getElementById('answer1');
         displaystatus.style.color="red";
         displaystatus.innerHTML="Wrong";
+        keyw=0;
     }
     document.getElementById('correctanswer').innerHTML = `Correct answer : ${answers.get(randomNo)}`;
  
     let nextButton=document.getElementById("nextButton");
     nextButton.removeAttribute("disabled");
+    addDescription(keyw);
+}
+
+//for setting the description
+
+function addDescription(keyw)
+{
+    console.log(keyw,"check")
+    if(keyw===1)
+    {   let audio1 = document.getElementById('successans');
+        audio1.play()
+        document.querySelector('#description').textContent="GOOD Work! "+descriptions.get(randomNo);
+    }
+    else
+    {   
+        let audio2 = document.getElementById('wrongans');
+        audio2.play()
+        document.querySelector('#description').textContent="ALAS! "+descriptions.get(randomNo);
+    }
+ 
 }
 
